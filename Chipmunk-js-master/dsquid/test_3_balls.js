9731932342be35237a43a49a9d8679cb870ddaba
Zachary Aman <zackyuaman@gmail.com>
2015-07-15 21:07:05 -0400

    basic filtering

--- Chipmunk-js-master/dsquid/test_3_balls.js
+++ Chipmunk-js-master/dsquid/test_3_balls.js
@@ -15,6 +15,8 @@ var undo_stack = [];
 
 var datapoints = [];
 
+var filter_list = [];
+
 var colorDim;
 var sizeDim;
 var xDim;
@@ -141,7 +143,24 @@ function set_scale(){
     //console.log("yScale: "+yScale);
 }
 
+function get_scale(header){
+    var min = datapoints[0].fields[header];
+    var max = datapoints[0].fields[header];
+
+    for(var i = 0; i < datapoints.length; i++){
+        if(datapoints[i].fields[header] > max){
+            max = datapoints[i].fields[header];
+        }
+        if(datapoints[i].fields[header] < min){
+            min = datapoints[i].fields[header];
+        }
+    }
+    return {min: min, max: max};
+}
+
 function position_points() {
+
+    console.log("position points");
 
     var x_var = xDim;
     var y_var = yDim;
@@ -158,11 +177,29 @@ function position_points() {
         var yPos = datapoints[i].fields[y_var];
         datapoints[i].moveTarget(xPos * 580/xScale + 20, yPos * 380/ yScale + 20);
 
+        datapoints[i].enabled = true;
+
+        //check filters
+        for(var j = 0; j < filter_list.length; j++){
+            if(typeof(filter_list[j]) != "undefined"){
+                if(filter_list[j].filter_point(datapoints[i]) == false){
+                    datapoints[i].enabled = false;
+                }
+            }
+        }
+
         //set color
         if(typeof(color_var) != "undefined"){
             //point.body.style()
             var colorPos = datapoints[i].fields[color_var];
+            if(datapoints[i].enabled == true && filter_list.length > 0){
             var colorString = "rgb("+Math.round(colorPos/colorScale * 255)+","+Math.round(colorPos/colorScale * 255)+","+Math.round(colorPos/colorScale * 255)+")";
+                var colorString = "rgb(255,0,0)";
+            }
+            else{
+                var colorString = "rgba("+Math.round(colorPos/colorScale * 255)+","+Math.round(colorPos/colorScale * 255)+","+Math.round(colorPos/colorScale * 255)+","+.2+")";
+                //var colorString = "rgb(255,0,0)";
+            }
             //console.log(colorString);
 
 
@@ -244,6 +281,8 @@ function open_filters() {
         //if temp != current axes
         //push command object
 
+        //populate the filter list from filters[]
+
     }
     else {
         filters.css({"display": "block"});
@@ -255,7 +294,7 @@ function open_filters() {
 function add_filter() {
     //create new select
 
-    var newFilter = $('<div><select onchange="filter_update_axes(this)">Axes</select> Min: <input type="number"  onchange="filter_update_numbers(this)"> Max: <input type="number" onchange="filter_update_numbers(this)"><span onclick="remove_filter(this)"> Remove</span></div>')
+    var newFilter = $('<div filter_id="'+filter_list.length+'"><select onchange="filter_update_axes(this)">Axes</select> Min: <input type="number"  onchange="filter_update_numbers(this)"> Max: <input type="number" onchange="filter_update_numbers(this)"><span onclick="remove_filter(this)"> Remove</span></div>')
     $("#filters").append(newFilter);
 
     newSelect = newFilter.children("select")[0];
@@ -264,27 +303,115 @@ function add_filter() {
     for (var i = 1; i < headers.length; i++) {
         newSelect.options[i - 1] = new Option(headers[i], headers[i]);
         //ySelect.options[i - 1] = new Option(headers[i], headers[i]);
+
+
+
+        //onchange -> redraw
+
+        //<select id="xAxis" onchange="position_points();"></select>
+
+
+
+    }
+    var scale = get_scale(headers[1]);
+    //populate values in min / max
+    min_field = newFilter.children("[type='number']")[0];
+    max_field = newFilter.children("[type='number']")[1];
+
+    $(min_field).val(scale.min);
+    $(max_field).val(scale.max);
+
+    //create new filter
+    //console.log(new_filter("Sector #", 5, 8));
+
+    //push new filter to filters[]
+    filter_list.push(new_filter("Sector #", scale.min, scale.max));
+
+    //allow change through interface to edit the filter in filter[]
+
+    //filter
+}
+
+var new_filter = function(header, min, max){
+    var filter = {};
+    filter.header = header;
+    filter.min = min;
+    filter.max = max;
+
+    //return boolean
+    //if datapoint[header] within bounds of min / max, return true
+    //else, return false
+    filter.filter_point = function(datapoint){
+        //console.log(datapoint.fields[this.header]);
+        if(datapoint.fields[this.header] <= this.max && datapoint.fields[this.header] >= this.min){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 
-    //populate values in min / max
-
-    //onchange -> redraw
-
-    //<select id="xAxis" onchange="position_points();"></select>
+    return filter;
 }
+
+//var test_filter = new_filter("Sector #", 5, 8);
+
+
 function remove_filter(a){
     console.log("remove filter");
+    var filter_id = $(a).parent().attr("filter_id");
+
+    delete filter_list[filter_id];
+
+
     $(a).parent().remove();
+
+    //redraw
 }
 
 function filter_update_axes(a){
+
+
+    //get parent for filter index
+    var filter_parent = $(a).parent();
+    var filter_id = filter_parent.attr("filter_id");
+
+
+    //set which header field the filter is using
+    filter_list[filter_id].header = $(a).val();
+
     //update the min / max values
+    var min_max = get_scale($(a).val());
+
+    filter_list[filter_id].min = min_max.min;
+    filter_list[filter_id].max = min_max.max;
+
+    min_field = filter_parent.children("[type='number']")[0];
+    max_field = filter_parent.children("[type='number']")[1];
+
+    $(min_field).val(min_max.min);
+    $(max_field).val(min_max.max);
+
+    //console.log(filter_list);
+
+    //redraw
+    position_points();
 
 }
 function filter_update_numbers(a){
-    //
+    var filter_parent = $(a).parent();
+    var filter_id = filter_parent.attr("filter_id");
+
+    min_field = filter_parent.children("[type='number']")[0];
+    max_field = filter_parent.children("[type='number']")[1];
+
+    filter_list[filter_id].min = $(min_field).val();
+    filter_list[filter_id].max = $(max_field).val();
+
+    //console.log(filter_list);
 
     //redraw
+    position_points();
 }
 
 var Balls = function () {
@@ -494,3 +621,9 @@ Balls.prototype = Object.create(Test.prototype);
 balls = new Balls();
 balls.run();
 
+// Array Remove - By John Resig (MIT Licensed)
+Array.prototype.remove = function(from, to) {
+    var rest = this.slice((to || from) + 1 || this.length);
+    this.length = from < 0 ? this.length + from : from;
+    return this.push.apply(this, rest);
+};
